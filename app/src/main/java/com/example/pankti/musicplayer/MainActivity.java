491f83ba8079910a2c44e54e16d602dfc345ee5b
panktisugandhi <panktisugandhi143@gmail.com>
2017-02-17 08:31:55 +0530

    Complete Task-10

--- app/src/main/java/com/example/pankti/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/pankti/musicplayer/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
 //        btn.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
